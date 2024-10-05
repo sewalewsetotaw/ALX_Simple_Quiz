7ad24ed96a5985f3f4923247eeb9f2fdc3165b00
sewalewsetotaw <sewalews29@gmail.com>
2024-10-06 01:41:43 +0300

    fixing strict equality

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     let choice = document.querySelector('input[name="quiz"]:checked');  
     let userAnswer = choice.value; 
     let feedback=document.getElementById('feedback');
-   if(userAnswer == correctAnswer){
+   if(userAnswer === correctAnswer){
     feedback.textContent="Correct! Well done.";
    }
    else{
